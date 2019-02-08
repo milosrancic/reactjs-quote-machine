32bea9e44a04c6a10502a9f1a00ef0a2edd6fb71
Milos <milosrancic94@gmail.com>
2019-02-08 18:55:38 +0100

    Updates

---
+++ precache-manifest.dd045e7f1adab864b39ecd046d58335e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83e15de14a04f29cdc15",
+    "url": "/reactjs-quote-machine/static/css/main.c3310926.chunk.css"
+  },
+  {
+    "revision": "83e15de14a04f29cdc15",
+    "url": "/reactjs-quote-machine/static/js/main.83e15de1.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/reactjs-quote-machine/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "81c2c6432ba58a21d20e",
+    "url": "/reactjs-quote-machine/static/js/runtime~main.81c2c643.js"
+  },
+  {
+    "revision": "b2d137af7bcf7b2e7fed6141fd03e610",
+    "url": "/reactjs-quote-machine/index.html"
+  }
+];
